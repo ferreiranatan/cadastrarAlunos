35dd65e27ec7ca1b64328a620af8dedfde443331
ferreiranatan <natan_ael@hotmail.com>
2024-07-16 17:37:46 -0300

    ajustes em listar e cadastrar

--- src/cadastrarAluno.ps1
+++ src/cadastrarAluno.ps1
@@ -2,39 +2,44 @@
 Import-Module ActiveDirectory
 
 # Caminho do arquivo CSV
-$csvPath = "..\csv\da12.csv"
-
+$csvPath = "C:\Users\natanael.ferreira\Desktop\script_cadastro\csv\da12.csv"
 $users = Import-Csv $csvPath
 
 # Unidade Organizacional alvo, dependendo da unidade alterar o valor do valor de $targetOU
 $targetOU = "OU=Alunos,DC=digitalcollegesul,DC=local"
 
 # Loop para acessar cada usuário no CSV
-foreach($user in $users) {
+foreach ($user in $users) {
     # Nome completo do usuário
-    $nomeCompleto = $user.nome
+    $nomeCompleto = $user.Nome
     # Primeiro nome do usuário
     $firstName = ($user.Nome -split ' ')[0]
-    # CPF sem pontos
-    $cpfSemPonto = $user.CPF -replace '\.', ''
+    # CPF sem pontos e hífens
+    $cpfSemFormatacao = $user.cpf -replace '\.|-', ''
     # Primeiros 4 dígitos do CPF
-    $cpfUser = $cpfSemPonto.Substring(0,4)
+    $cpfUser = $cpfSemFormatacao.Substring(0, 4)
     # Nome de usuário
     $userName = ($firstName + $cpfUser)
 
-    # Verifica se o usuário já existe caso não exista, cria um novo usuário
-    if (-not (Get-ADUser -Filter {SamAccountName -eq $userName})) {
+    # Verifica se o usuário já existe, caso não exista, cria um novo usuário
+    if (-not (Get-ADUser -Filter { SamAccountName -eq $userName })) {
         New-ADUser -Name $user.Nome -SamAccountName $userName `
-        -UserPrincipalName ($userName + "@digitalcollegesul.local") `
-        -AccountPassword (ConvertTo-SecureString -AsPlainText $cpfSemPonto -Force) `
-        -Path $targetOU -Enabled $true -ChangePasswordAtLogon $true `
-        -PasswordNeverExpires $false
+            -UserPrincipalName ($userName + "@digitalcollegesul.local") `
+            -AccountPassword (ConvertTo-SecureString -AsPlainText $cpfSemFormatacao -Force) `
+            -Path $targetOU -Enabled $true -ChangePasswordAtLogon $true `
+            -PasswordNeverExpires $false
         Write-Host "Usuario $nomeCompleto criado com sucesso."
-    } else {
-        # se o usuário já existir, atualiza a senha
-        $securePassword = ConvertTo-SecureString -AsPlainText $cpfSemPonto -Force
-        Set-ADAccountPassword -Identity $userName -AccountPassword $securePassword -Reset
-        Write-Host "Usuario $nomeCompleto já existe, a senha foi atualizada."
-       
-}
+    }
+    else {
+        # Se o usuário já existir, atualiza a senha
+        $securePassword = ConvertTo-SecureString -AsPlainText $cpfSemFormatacao -Force
+        Set-ADAccountPassword -Identity $userName -NewPassword $securePassword -Reset
+
+        # Atualiza o status da conta para garantir que está habilitada e requer a troca de senha no primeiro logon
+        Set-ADUser -Identity $userName `
+            -Enabled $true -ChangePasswordAtLogon $true `
+            -PasswordNeverExpires $false
+        
+        Write-Host "A senha do usuario $nomeCompleto foi alterada com sucesso."
+    }
 }
--- src/listaAlunos.ps1
+++ src/listaAlunos.ps1
@@ -1,28 +1,25 @@
 # Caminho para o arquivo CSV
 $csvPath = "C:\Users\natanael.ferreira\Desktop\script_cadastro\csv\da12.csv"
 
+
 # Importa os dados do CSV
 $users = Import-Csv $csvPath
 
 
 foreach($user in $users){
     #Pega nome completo
-    $nomeCompleto = $user.nome
+    $nomeCompleto = $user.Nome
     # Pega o primeiro nome a partir do primeiro espaço
-    $firstName = ($user.nome -split ' ')[0]
+    $firstName = ($user.Nome -split ' ')[0]
 
     # Remove pontos e hífen do CPF
     $cpfSemFormatacao = $user.cpf -replace '\.|-', ''
     
     # Pega os primeiros quatro caracteres do CPF
-
-    $cpfUser = $cpfSemFormatacao.Substring(0, 4)
-
+    $cpfUser = $cpfSemFormatacao.substring(0, 4)
     
     # Concatena o primeiro nome com os primeiros quatro caracteres do CPF
     $userName = $firstName + $cpfUser
     
-
     Write-Output $nomeCompleto $userName $cpfSemFormatacao $user
-
 }
